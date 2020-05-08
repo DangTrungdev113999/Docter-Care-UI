eb6e2449e77f6762f24d4cd8d4f50e148a5c81d8
dangtrungdev <dangtrungdev113999@gmail.com>
2020-05-08 23:32:19 +0700

    set up bottom bar

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import {Text, Block} from './src/lib';
+import {} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
 import HomeScreen from './src/screens/Home';
 import SearchScreen from './src/screens/Search';
+import Icon from './src/components/Icon';
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainTab = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        tabBarIcon={
+          <Icon type="entypo" name="map" size={30} color="#fff" mr={20} />
+        }
+      />
+      <Tab.Screen name="Search" component={SearchScreen} />
+    </Tab.Navigator>
+  );
+};
 
 const App = () => {
-  return <SearchScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator headerMode="none">
+        <Stack.Screen name="MainTab" component={MainTab} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
